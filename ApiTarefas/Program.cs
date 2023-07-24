80b15432e99492c7a2e586f4126d7ceabac3f368
Bruno Ricardo Carmena <106131265+brunocarmena57@users.noreply.github.com>
2023-07-23 21:44:07 -0300

    Actualizar Program.cs

--- ApiTarefas/Program.cs
+++ ApiTarefas/Program.cs
@@ -5,7 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the ontainer.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseInMemoryDatabase("TarefasDB"));
 
